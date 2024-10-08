e9a7c868ebc5440c32fcc09ce3d0311cbeff4a95
Theodosiusss <1202@htl.rennweg.at>
2024-10-08 21:01:25 +0200

    8,9

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
+
 <!doctype html>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -13,15 +15,38 @@
     <h1>Tolle Videos</h1>
 </header>
 <main id="mainContainer">
+    <?php include "./Video.php";
+    $a = new YoutubeVideo("https://www.youtube.com/embed/vkIsEl-gdTA?si=o4PJD7NubqjZVMF3","life is a name");
+    $b = new YoutubeVideo("https://www.youtube.com/embed/vkIsEl-gdTA?si=o4PJD7NubqjZVMF3","life is a name");
+    $c = new YoutubeVideo("https://www.youtube.com/embed/vkIsEl-gdTA?si=o4PJD7NubqjZVMF3","life is a name");
+    $d = new YoutubeVideo("https://www.youtube.com/embed/vkIsEl-gdTA?si=o4PJD7NubqjZVMF3","life is a name");
+    $e = new YoutubeVideo("https://www.youtube.com/embed/vkIsEl-gdTA?si=o4PJD7NubqjZVMF3","life is a name");
+
+    echo "<div class=\"divVideos\" >".$a->getHtmlcode().$b->getHtmlcode().$c->getHtmlcode().$d->getHtmlcode().$e->getHtmlcode()."</div>"
+
+    ?>
+    <?php
+    $a = new VimeoVideo("https://player.vimeo.com/video/1006042481?h=3e440200f9","aiaiiai");
+    $b = new VimeoVideo("https://player.vimeo.com/video/1006042481?h=3e440200f9","uiuiui");
+    $c = new VimeoVideo("https://player.vimeo.com/video/1006042481?h=3e440200f9","wewewew is a name");
+    $d = new VimeoVideo("https://player.vimeo.com/video/1006042481?h=3e440200f9","griaß euch");
+    $e = new VimeoVideo("https://player.vimeo.com/video/1006042481?h=3e440200f9","hohohoh");
+
+    echo "<div class=\"divVideos\" >".$a->getHtmlcode().$b->getHtmlcode().$c->getHtmlcode().$d->getHtmlcode().$e->getHtmlcode()."</div>"
+
+    ?>
     <div class="divVideos">
+
         <iframe width="560" height="315" src="https://www.youtube.com/embed/zm-8Jxdj0Fk?si=9CQC5P28esM6spjP"
                 title="YouTube video player" frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
         <iframe width="560" height="315" src="https://www.youtube.com/embed/zm-8Jxdj0Fk?si=9CQC5P28esM6spjP"
                 title="YouTube video player" frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
         <iframe width="560" height="315" src="https://www.youtube.com/embed/zm-8Jxdj0Fk?si=9CQC5P28esM6spjP"
                 title="YouTube video player" frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
