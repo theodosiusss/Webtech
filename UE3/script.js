f6ca9ba0f4efac8759dbcffdbc540c65a46c7476
Theodosiusss <1202@htl.rennweg.at>
2024-12-11 21:39:22 +0100

    us5 optional ready

--- UE3/script.js
+++ UE3/script.js
@@ -2,6 +2,8 @@ let weddingButton = document.getElementById("buttonGetRandomWedding");
 let randomWedding = document.getElementById("randomWedding");
 let jsonArray;
 
+let mapElement = document.getElementById("osm-map");
+
 
 fetch("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TRAUMHOCHZEITOGD&srsName=EPSG:4326&outputFormat=json")
     .then((resp) => resp.json())
@@ -9,11 +11,27 @@ fetch("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=
         console.log(object);
         jsonArray = object.features;
 
-    })
+    });
+
+
+let map = L.map(mapElement).setView([48.2082, 16.3738], 13); // Initialisierung auf Wien
+
+L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+let markersGroup = L.layerGroup().addTo(map);
+
+
+
+
+
 
 
 weddingButton.onclick = function () {
+
     let randomNumber = Math.floor(Math.random() * jsonArray.length);
+
 
     console.log(randomNumber);
     console.log(jsonArray[randomNumber].properties);
@@ -22,6 +40,23 @@ weddingButton.onclick = function () {
         "<br>Name: " + jsonArray[randomNumber].properties.LOCATION +
         "<br>Tel: " + jsonArray[randomNumber].properties.TELEFONNUMMER
     ;
+
+
+
+    console.log(jsonArray[randomNumber].geometry.coordinates[0])
+
+
+    let target = L.latLng(jsonArray[randomNumber].geometry.coordinates[1], jsonArray[randomNumber].geometry.coordinates[0]);
+
+
+    map.setView(target, 14);
+
+    markersGroup.clearLayers();
+
+    L.marker(target).addTo(markersGroup);
 };
+
+
+
 
 
